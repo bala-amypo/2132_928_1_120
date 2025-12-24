26e794ef899ff300ab3a150d616b570f27b751da
bala-amypo <bala@amypo.in>
2025-12-24 07:47:35 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterResponseDto.java
+++ src/main/java/com/example/demo/dto/RegisterResponseDto.java
@@ -5,14 +5,28 @@ public class RegisterResponseDto {
     private Long userId;
     private String email;
     private String role;
+    private String token;
 
-    public RegisterResponseDto(Long userId, String email, String role) {
+    public RegisterResponseDto(Long userId, String email, String role, String token) {
         this.userId = userId;
         this.email = email;
         this.role = role;
+        this.token = token;
     }
 
-    public Long getUserId() { return userId; }
-    public String getEmail() { return email; }
-    public String getRole() { return role; }
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getToken() {
+        return token;
+    }
 }
