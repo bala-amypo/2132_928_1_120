2de2b2c185389f436f3f43b436da308272ad8d25
bala-amypo <bala@amypo.in>
2025-12-25 12:25:11 +0000

    User commit

--- src/main/java/com/example/demo/dto/CountResponseDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-public class CountResponseDto {
-    private long count;
-    public CountResponseDto(long count) { this.count = count; }
-    public long getCount() { return count; }
-}
