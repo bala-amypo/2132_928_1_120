a0667f191af35521157d59ff5d24123b70b92f4d
bala-amypo <bala@amypo.in>
2025-12-25 15:31:11 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,24 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("Encrypted password = " + encoder.encode("welcome123"));
-
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    // 🔴 TEMPORARY – remove after generating password
+    @Bean
+    CommandLineRunner passwordGenerator() {
+        return args -> {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            System.out.println("HASH = " + encoder.encode("welcome123"));
+        };
     }
 }
